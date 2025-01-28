03900cbbe3fdcc7cd2b7a7bd9815d682a60fc35b
Abdellatif Nasser-Allah <abdnasse@c3r4p9.1337.ma>
2025-01-28 20:51:01 +0100

    just started with the main function in pushswap file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,15 +6,19 @@
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 17:08:22 by abdnasse          #+#    #+#             */
-/*   Updated: 2025/01/27 17:41:34 by abdnasse         ###   ########.fr       */
+/*   Updated: 2025/01/28 15:15:28 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+#include "ft_printf.h"
+#include "libft.h"
+
 typedef	struct	 s_list{
 	void	*content;
 	struct	s_list	*next;
+}	t_list;
 
 #endif
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,17 +6,65 @@
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 17:07:39 by abdnasse          #+#    #+#             */
-/*   Updated: 2025/01/27 17:42:23 by abdnasse         ###   ########.fr       */
+/*   Updated: 2025/01/28 16:02:00 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "push_swap.h"
+
+int	fill stack(t_list **list, int ac, char **av);
+int	check_error(char *str);
 
 int	main(int ac, char **av)
 {
+	t_list	*a;
+	t_list	*b;
 
 	if (ac != 1)
 	{
-		
+		//Fill the stack a
+		a = NULL;
+		b = NULL;
+		if (!fill_stack(&a, ac, av))
+			return (1);
+
+	//TODO: the algorithm to sort the stack a 
 	}
+}
+
+int	fill stack(t_list **list, int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while( i < ac)
+	{
+		j = 0;
+		while(av[i][j])
+		{
+			if (check_error(av[i]))
+			{
+				write(2, "Error\n", 6);
+				return (0);
+			}
+			//TODO: error if the number is dublicated
+			j++;
+		}
+		i++;
+	}
+}
+
+
+int	check_error(char *str)
+{
+	while (*str)
+	{
+		//TODO: error if the number is not intergers
+		if(*str
+
+		//TODO: error if the number is bigger that the limits of the Ingteger
+		str++;
+	}
+
 }
